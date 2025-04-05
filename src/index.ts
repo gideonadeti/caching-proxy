790a5fad98808b37aa3ca6fb675b45f5cf7c2062
gideonadeti <gideonadeti0@gmail.com>
2025-04-05 18:56:51 +0000

    refactor: improve cache clearing logic in CLI options
    Updated the cache clearing logic to streamline the command handling and ensure consistent behavior when clearing the cache.

--- src/index.ts
+++ src/index.ts
@@ -10,29 +10,30 @@ const program = new Command();
 program
   .name("caching-proxy")
   .description(
-    "A CLI tool that starts a caching proxy server, forwards requests to an actual server, and cache the responses."
+    "A CLI tool that starts a caching proxy server, forwards requests to an actual server, and caches the responses."
   )
   .option("-p, --port <number>", "Port to run the server on", "3000")
-  .option("-o, --origin <url>", "Origin URL to proxy requests to");
-
-program
-  .command("clear-cache")
-  .description("Clear in-memory cache")
-  .action(() => {
+  .option("-o, --origin <url>", "Origin URL to proxy requests to")
+  .option("--clear-cache", "Clear the in-memory cache", () => {
     clearCache();
 
     console.log("Cache cleared");
+
+    process.exit(0);
   });
 
 program.action(async (options) => {
-  const port = parseInt(options.port, 10);
+  // If --clear-cache is set, it will already have cleared the cache and exited
+  if (!options.clearCache) {
+    if (!options.origin) {
+      console.error("Missing --origin");
+      process.exit(1);
+    }
 
-  if (!options.origin) {
-    console.error("Missing --origin");
-    process.exit(1);
+    const port = parseInt(options.port, 10);
+
+    await startServer(port, options.origin);
   }
-
-  await startServer(port, options.origin);
 });
 
 program.parse();
